0ccc1b782c34d7a8b34a3dd8ed07a88a88226b7c
erikazr <erikazael47@gmail.com>
2026-07-19 18:59:13 -0600

    Request validation added in Account.java

--- src/main/java/com/example/spring_data/model/Account.java
+++ src/main/java/com/example/spring_data/model/Account.java
@@ -1,6 +1,7 @@
 package com.example.spring_data.model;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 
@@ -9,12 +10,15 @@ import java.math.BigDecimal;
 public class Account {
 
     @Id
+    @NotNull
     @Column("id")
     private Long id;
 
+    @NotBlank
     @Column("account_name")
     private String account_name;
 
+    @NotNull
     @Column("amount")
     private BigDecimal amount;
 
