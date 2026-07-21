1312bbea25937fb5bd9805841076b508dcf09045
erikazr <erikazael47@gmail.com>
2026-07-20 23:10:05 -0600

    Replacing Spring data JDBC with Spring Data JPA

--- src/main/java/com/example/spring_data/repository/AccountRepository.java
+++ src/main/java/com/example/spring_data/repository/AccountRepository.java
@@ -1,23 +1,23 @@
 package com.example.spring_data.repository;
 
 import com.example.spring_data.model.Account;
-import org.springframework.data.jdbc.repository.query.Modifying;
-import org.springframework.data.jdbc.repository.query.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
-public interface AccountRepository extends CrudRepository<Account, Long> {
+public interface AccountRepository extends JpaRepository<Account, Long> {
     @Modifying
     @Query("UPDATE account SET amount = :amount WHERE id=:accountId")
     void updateAmountById (Long accountId, BigDecimal amount);
 
-    @Query("SELECT * FROM account WHERE id = :accountId")
+    @Query("SELECT FROM account WHERE id = :accountId")
     Account findAccountById(Long accountId);
 
-    @Query("SELECT * FROM account WHERE account_name= :name")
+    @Query("SELECT FROM account WHERE account_name= :name")
     List<Account> findAccountByName(String name);
 }
