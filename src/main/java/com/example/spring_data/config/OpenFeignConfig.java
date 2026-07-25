b281d448fbd43421dff0cb15a14002b54b29d9ce
erikazr <erikazael47@gmail.com>
2026-07-24 23:58:48 -0600

    Adding jsonplaceholder url in application.properties

--- src/main/java/com/example/spring_data/config/OpenFeignConfig.java
+++ src/main/java/com/example/spring_data/config/OpenFeignConfig.java
@@ -1,8 +1,10 @@
 package com.example.spring_data.config;
 
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-
+@EnableFeignClients
 public class OpenFeignConfig {
+
 }
