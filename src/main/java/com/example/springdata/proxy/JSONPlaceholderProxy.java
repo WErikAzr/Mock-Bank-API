9813d50a6a9e9e0263d8cfa0d034b49f5ed240cd
erikazr <erikazael47@gmail.com>
2026-08-01 23:50:59 -0600

    adding another JSONPlaceholderProxy method

--- src/main/java/com/example/springdata/proxy/JSONPlaceholderProxy.java
+++ src/main/java/com/example/springdata/proxy/JSONPlaceholderProxy.java
@@ -3,6 +3,9 @@ package com.example.springdata.proxy;
 import com.example.springdata.model.JSONPlaceholderModel;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.List;
 
@@ -11,5 +14,6 @@ public interface JSONPlaceholderProxy {
     @GetMapping("/posts")
     List<JSONPlaceholderModel> listAllResources();
 
-
+    @PostMapping("/post")
+    void createResource(@RequestHeader String contentType, @RequestBody JSONPlaceholderModel jsonPlaceholderModel);
 }
